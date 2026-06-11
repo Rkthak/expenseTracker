cd01659376bd70b8f2cb2a91de61efe847611032
Rkthak <rkon2609@gmail.com>
2026-06-11 14:48:18 +0530

    added buttons in Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,11 @@
+import Btnsec from "../Sections/Btnsec";
 import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex flex-col md:flex-row gap-5 justify-between items-start md:items-center">
       <Logo />
+      <Btnsec />
     </div>
   );
 };
---
+++ src/Sections/Btnsec.jsx
@@ -0,0 +1,14 @@
+const Btnsec = () => {
+  return (
+    <div className="">
+      <button className="cursor-pointer px-4 py-2 rounded-2xl bg-slate-900 text-slate-50 font-['poppins'] font-semibold md:mx-8 mr-8">
+        Dark
+      </button>
+      <button className="cursor-pointer px-4 py-2 rounded-2xl bg-indigo-600 text-slate-50 font-['poppins'] font-semibold hover:scale-110 transition-transform">
+        + Add Expense
+      </button>
+    </div>
+  );
+};
+
+export default Btnsec;
